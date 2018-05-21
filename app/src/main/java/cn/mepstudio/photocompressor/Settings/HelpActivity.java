a729029e81273d7a0adbdf8468f806264fef2dfc
DoonZhang <DoonZhang@MacBook-Air.local>
2018-05-21 23:50:00 +0800

    第一版本提交

---
+++ app/src/main/java/cn/mepstudio/photocompressor/Settings/HelpActivity.java
@@ -0,0 +1,22 @@
+package cn.mepstudio.photocompressor.Settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.mepstudio.photocompressor.R;
+
+/**
+ * Created by ZhangDong on 2017/10/11.
+ * E-Mail: DoonZhang@qq.com
+ * Copyright (c) 2017。 ZhangDong All rights reserved.
+ */
+
+public class HelpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_help);
+
+
+    }
+}
---
+++ app/src/main/java/cn/mepstudio/photocompressor/Utils/LayoutUtil.java
@@ -0,0 +1,20 @@
+package cn.mepstudio.photocompressor.Utils;
+
+/**
+ * Created by ZhangDong on 2017/11/29.
+ * E-Mail: DoonZhang@qq.com
+ * Copyright (c) 2017。 ZhangDong All rights reserved.
+ */
+
+public class LayoutUtil {
+    //通过检测照片的宽高布局来决定照片展示方式
+    /**
+     * 高>=宽与宽等为左右布局
+     * 高<宽为上下布局
+     */
+    private void CompareLayout(){
+
+    }
+
+
+}
